53cf21ae79ac88f92c0c5fe4257cdd8c273bccc1
Saran V <101328434+saranv01@users.noreply.github.com>
2024-08-21 14:51:54 +0530

    Time: 4 ms (46.54%), Space: 7.8 MB (72.17%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -1,25 +1,16 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
-        string lastW;
-
         int j=s.size()-1;
         int i;
         while(s[j]==' '){
             j--;
         }
-
         for(i=j;i>=0;i--){
-            if(s[i]!=' '){
-                
-            }
             if(s[i]==' '){
                 break;
             }
         }
-       j++;
-       i++;
         return j-i;
-
     }
 };
