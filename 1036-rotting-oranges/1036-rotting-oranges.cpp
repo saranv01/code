e53fdce3a3c3785bb1326c1a13ef01f1bf98a07c
Saran V <101328434+saranv01@users.noreply.github.com>
2024-09-12 13:58:53 +0530

    Time: 3 ms (84.44%), Space: 17.2 MB (11.22%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,49 +1,50 @@
 class Solution {
+private:
+    int rotOrange(vector<int>delx,vector<int>dely,vector<vector<int>>&visited,int& rottenOranges,queue<pair<pair<int,int>,int>>q,int m, int n){
+        int time=0;
+        while(!q.empty()){
+            int f=q.front().first.first;
+            int s=q.front().first.second;
+            int t=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                    int x=f+delx[i];
+                    int y=s+dely[i];
+                    int temp=t;
+                    if(x<m&&x>=0 && y<n&&y>=0 &&visited[x][y]==1 ){
+                        temp++;
+                        time=max(time,temp);
+                        rottenOranges++;
+                        visited[x][y]=2;
+                        q.push({{x,y},temp});
+                    }
+               }
+        }    
+        return time;
+    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        if(grid.empty()) return 0;
-        int n=grid.size();
-        int m=grid[0].size();
-        int oranges=0;
+        int m =grid.size();
+        int n =grid[0].size();
+        vector<vector<int>>visited(m,vector<int>(n));
+        int initialFreshOrange=0;
         int rottenOranges=0;
-        int days=0;
-
-        queue<pair<int,int>>rotten;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]!=0) oranges++;
-                if(grid[i][j]==2){
-                    rotten.push({i,j});
-                }
+        queue<pair<pair<int,int>,int>>q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1)initialFreshOrange++;
+                if(grid[i][j]==2)q.push({{i,j},0});
+                visited[i][j]=grid[i][j];
             }
         }
+        vector<int>delx={0, 0, 1, -1};
+        vector<int>dely={1, -1, 0, 0};
 
-        int dx[4]={0,0,1,-1};
-        int dy[4]={1,-1,0,0};
-        while(!rotten.empty()){
-        int k=rotten.size();
-        rottenOranges+=k;
-        while(k--){
-         
-          
-            int x=rotten.front().first,y=rotten.front().second;
-            rotten.pop();
-            for(int i=0;i<4;i++){
-            
-                    int ny=y+dy[i],nx=x+dx[i];
-                    if(ny<0||nx<0||nx>n-1||ny>m-1||grid[nx][ny]!=1) continue;
-                    rotten.push({nx,ny});
-                    grid[nx][ny]=2;
-   
-            }
-        }
-        if(!rotten.empty()) days++;
-        }
-
-        if(rottenOranges==oranges) return days;
-        return -1;
+        int time= rotOrange(delx,dely,visited,rottenOranges,q,m,n);
+        return (initialFreshOrange==rottenOranges)?time:-1;
 
 
-        
+
+
     }
 };
