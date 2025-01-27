eb40e78b1dbbb0c07c6ecf78a18d61b9ff5eb393
Saran V <101328434+saranv01@users.noreply.github.com>
2025-01-28 02:45:11 +0530

    Time: 107 ms (31.55%), Space: 66.5 MB (71.67%) - LeetHub

--- 1558-course-schedule-iv/1558-course-schedule-iv.cpp
+++ 1558-course-schedule-iv/1558-course-schedule-iv.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
     vector<bool> checkIfPrerequisite(int numCourses, vector<vector<int>>& prerequisites, vector<vector<int>>& queries) {
-        //floyd warshal
         vector<vector<bool>>isReachable(numCourses,vector<bool>(numCourses,false));
         vector<bool>result;
         for(auto it:prerequisites){
@@ -15,75 +14,8 @@ public:
             }
         }
         for(int i=0;i<queries.size();i++){
-
             result.push_back(isReachable[queries[i][0]][queries[i][1]]);
         }
         return result;
-
-
-
-
-
-        // if(prerequisites.size()==0){
-        //     vector<bool>r(queries.size(),false);
-        //     return r;
-        // }
-        // vector<bool> result(queries.size());
-        // vector<vector<int>>adj(numCourses);
-        // vector<int>indegree(numCourses,0);
-        // vector<pair<int,int>>orders;
-        // queue<int>q;
-        // for(auto it:prerequisites){
-        //     adj[it[0]].push_back(it[1]);
-        //     indegree[it[1]]++;
-        // }
-        // for(int i=0;i<numCourses;i++){
-        //     if(indegree[i]==0){
-        //         q.push(i);
-        //         orders.push_back({i,0});
-        //     }
-        // }
-        // int k=0;        
- 
-        // while(!q.empty()){
-        //     k++;
-        //     int x=q.size();
-        //     while(x--){
-
-            
-        //     int node=q.front();
-        //     // orders.push_back(node);
-        //     q.pop();
-        //     for(auto it:adj[node]){
-        //         int a=it;
-        //         indegree[a]--;
-        //         if(indegree[a]==0){
-        //             q.push(a);
-        //             orders.push_back({a,k});
-        //         }
-        //     }
-        //     }
-        // }
-        // for(int i=0;i<queries.size();i++){
-        //     if(orders.size()!=numCourses){
-        //         result[i]=false;
-        //         continue;  
-        //     }
-        //     else{
-        //         pair<int,int>f={-1,-1};
-        //         pair<int,int>s={-1,-1};
-        //         for(int j=0;j<orders.size();j++){
-        //             if(queries[i][0]==orders[j].first){
-        //                 f={j,orders[j].second};
-        //             }
-        //             if(queries[i][1]==orders[j].first){
-        //                 s={j,orders[j].second};
-        //             }
-        //         }
-        //         if(f.first<s.first&&f.second<s.second)result[i]=true;
-        //         else result[i]=false;
-        //     }
-        // }
-        // return result;
     }
 };
