f5e4eeb7416525aaac446e14b9127cf85f325c8f
Saran V <101328434+saranv01@users.noreply.github.com>
2025-02-21 00:36:48 +0530

    Time: 14 ms (64.51%), Space: 17.5 MB (25.72%) - LeetHub

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+    unordered_map<int,int>mp;
+    vector<vector<int>>dist(n,vector<int>(n,1e7));
+    for(auto it:edges){
+        dist[it[0]][it[1]]=it[2];
+        dist[it[1]][it[0]]=it[2];
+    }
+    for(int i=0;i<n;i++){
+        dist[i][i]=0;
+    }
+     for(int k=0;k<n;k++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                dist[i][j]=min(dist[i][j],(dist[i][k]+dist[k][j]));
+            }
+        }  
+     }
+     for(int i=0;i<n;i++){
+        int flg=0;
+        for(int j=0;j<n;j++){
+            if(dist[i][j]<=distanceThreshold&&i!=j){
+                mp[i]++;
+                mp[j]++;
+            }
+             if(i!=j&&dist[i][j]<=distanceThreshold){
+                flg=1;
+            }
+        }
+        if(flg==0)mp[i]=0;
+     }
+     
+     vector<pair<int,int>>vec(mp.begin(),mp.end());
+     sort(vec.begin(),vec.end(),[](const pair<int,int>&a, const pair<int,int>&b){
+        if(a.second==b.second){
+            return (a.first>b.first);
+        }
+        return a.second<b.second;
+     });
+     for(auto it:vec){
+        cout<<it.first<<" "<<it.second<<endl;
+     }
+    return vec[0].first;
+    }
+};
