bf08041b193b7ffe23271514a9fabb891142d1b7
Saran V <101328434+saranv01@users.noreply.github.com>
2025-02-24 22:57:37 +0530

    Time: 119 ms (16.83%), Space: 54.7 MB (9.85%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
-    bool solve(vector<int>&nums,int pos,vector<int>&dp){
-        if(dp[pos]!=-1)return dp[pos];
-        if(pos>=nums.size()-1)return true;
-        for(int i=1;i<=nums[pos];i++){
-            if(solve(nums,pos+i,dp))return dp[pos]=1;
-        }
-        return dp[pos]=0;
-    }
     bool canJump(vector<int>& nums) {
-        vector<int>dp(1e6,-1);
-       return solve(nums,0,dp);
+        vector<int>memo(nums.size(),false);
+        memo[nums.size()-1]=true;
+        for(int i=nums.size()-2;i>=0;i--){
+            for(int j=1;j<=nums[i];j++){
+                if((i+j)<nums.size()){
+                    if(memo[i+j]){
+                        memo[i]=true;
+                        break;
+                    }
+                }
+            }
+        }
+        return memo[0];
     }
 };
