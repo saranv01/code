00f2e361acb66e6175ac82478e109957b99fab17
Saran V <101328434+saranv01@users.noreply.github.com>
2025-02-24 22:15:45 +0530

    Time: 206 ms (7.35%), Space: 93.3 MB (5.01%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,33 +1,30 @@
 class Solution {
 public:
-    int solve(vector<int>&coins,int amount, int pos,vector<vector<int>>&dp){
-        if(dp[amount][pos]!=-1){
-            return dp[amount][pos];
-        }
-
-        if(pos==0){
-            if(amount%coins[0]==0){
-                return amount/coins[0];
-            }
-            else{
-                return 1e9;
-            }
-        }
-
-        int pick=1e9;
-        if(amount>=coins[pos]){
-            pick=1+solve(coins,amount-coins[pos],pos,dp);
-        }
-
-        int notPick=1e9;
-        notPick=0+solve(coins,amount,pos-1,dp);
-        
-        return dp[amount][pos]= min(pick,notPick);
-    }
     int coinChange(vector<int>& coins, int amount) {
         sort(coins.begin(),coins.end());
-        vector<vector<int>>dp(amount+1,vector<int>(coins.size(),-1));
-        int ans= solve(coins,amount,coins.size()-1,dp);
+        vector<vector<int>>memo(amount+1,vector<int>(coins.size(),1e9));
+        for(int i=0;i<coins.size();i++){
+            memo[0][i]=0;
+        }
+        for(int i=1;i<=amount;i++){
+            if(i%coins[0]==0){
+                memo[i][0]=i/coins[0];
+            }
+            else
+            memo[i][0]=1e9;
+        }
+        for(int amt=1;amt<amount+1;amt++){
+            for(int pos=1;pos<coins.size();pos++){
+                int pick=1e9;
+                if(amt>=coins[pos]){
+                    pick=1+memo[amt-coins[pos]][pos];
+                }
+                int notPick=1e9;
+                notPick=0+memo[amt][pos-1];
+                memo[amt][pos]= min(pick,notPick);               
+            }
+        }
+        int ans=memo[amount][coins.size()-1];
         if(ans==1e9)return -1;
         return ans;
     }
