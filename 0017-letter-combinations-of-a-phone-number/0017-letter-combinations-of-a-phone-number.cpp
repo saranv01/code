c01d73adad927557ae989ec0a6ea4a91c0b49d75
Saran V <101328434+saranv01@users.noreply.github.com>
2025-03-05 09:39:48 +0530

    Time: 2 ms (17.6%), Space: 9.3 MB (57.29%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     vector<string> letterCombinations(string digits) {
-            
             if(digits.size()<1)return{};
-            
             vector<string>buttons={"abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
             vector<string>ans={""};
-            
             for(auto digit:digits){
                 vector<string>temp;
                 for(auto letter:buttons[digit-'0'-2]){
-                    
                     for(auto it:ans){
                         temp.push_back(it+letter);
                     }
